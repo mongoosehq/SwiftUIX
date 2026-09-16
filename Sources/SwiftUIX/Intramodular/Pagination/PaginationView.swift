40ac7cd28de3ad703c9c30224d890e6a435800b3
Nate de Jager <dejager@gmail.com>
2026-09-16 09:24:17 -0700

    Fix PaginationView initialization with Xcode 27
    Remove `@inlinable` from the initializer to avoid a linkage error in `@State` macro-generated code.

--- Sources/SwiftUIX/Intramodular/Pagination/PaginationView.swift
+++ Sources/SwiftUIX/Intramodular/Pagination/PaginationView.swift
@@ -59,7 +59,6 @@ public struct PaginationView<Page: View>: View {
     
     var paginationState: Binding<PaginationState>?
     
-    @inlinable
     public init(
         content: AnyForEach<Page>,
         axis: Axis = .horizontal,
